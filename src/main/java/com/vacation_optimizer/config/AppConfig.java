3bd283b569554e1012c9701f45304718f0eaa741
Davi Santtana <batatovisk0330@gmail.com>
2026-08-26 15:22:33 -0300

    fix: add vercel domain to cors

--- src/main/java/com/vacation_optimizer/config/AppConfig.java
+++ src/main/java/com/vacation_optimizer/config/AppConfig.java
@@ -19,7 +19,7 @@ public class AppConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173", "https://vacation-optimizer-frontend.vercel.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
